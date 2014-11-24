8c3844d849f54e544964b38cacd1774c27c18431
Jason Mehring <nrgaway@gmail.com>
2014-11-24 17:19:28 -0500

    changed debug instructions

--- src/wing-debug-setup.sh
+++ src/wing-debug-setup.sh
@@ -12,16 +12,11 @@
 
 # Or just run a highstate
 # Debug -> Debug Environment -> Environment --> --local state.highstate -l debug
+#
 
-ln -sf /srv/salt-formulas/yamlscript-formula/src/yamlscript.py /var/cache/salt/minion/extmods/renderers/
-ln -sf /srv/salt-formulas/yamlscript-formula/src/yamlscript_utils.py /var/cache/salt/minion/extmods/utils/
-ln -sf /srv/salt-formulas/yamlscript-formula/src/voluptuous.py /var/cache/salt/minion/extmods/utils/
+# IMPORTANT NOTE.  Be sure we are using the rootfs version, not gitfs or paths will be incorrect!
+# Set breakpoint on /var/cache/salt/minion/extmods/...
 
-mkdir -p /srv/salt/_renderers/
-mkdir -p /srv/salt/_utils/
-ln -sf /srv/salt-formulas/yamlscript-formula/src/yamlscript.py /srv/salt/_renderers/
-ln -sf /srv/salt-formulas/yamlscript-formula/src/voluptuous.py /srv/salt/_utils/
-ln -sf /srv/salt-formulas/yamlscript-formula/src/yamlscript_utils.py /srv/salt/_utils/
-
-ln -s /srv/salt-formulas/yamlscript-formula/tests/users /srv/salt/users-test
-ln -sf /srv/salt-formulas/yamlscript-formula/tests/users/pillar /srv/pillar/users-test
+ln -sf /var/cache/salt/minion/extmods/renderers/yamlscript.pyc .
+ln -sf /var/cache/salt/minion/extmods/utils/yamlscript_utils.pyc .
+ln -sf /var/cache/salt/minion/extmods/utils/voluptuous.pyc .
