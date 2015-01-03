c9b6fa67b34aa0696cbf81d6ba9982dd84ee2b05
Jason Mehring <nrgaway@gmail.com>
2015-01-02 22:03:27 -0500

    Revert "Reworked WingIDE debugger setup to be more reliable"
    This reverts commit b9df93e5c016c76ef7e48999e82301cd8d669bb6.

--- src/wing-debug-setup.sh
+++ src/wing-debug-setup.sh
@@ -1,81 +1,33 @@
 #!/bin/bash
 
-################################################################################
-#
-# STEP 1:
-# -------
-# - Run this file from src directory
-#
-# STEP 2:
-# -------
-# - In WingIDE make sure salt-call.py is set as 'Main Debug File'
-#     Project -> Add Existing Directory ... /srv/salt-formulas
-#     Project -> Project Properties -> Debug/Execute -> Main Entry Point -> /srv/salt-formulas/yamlscript-formula/src/salt-call
-# - In salt-call.py make sure SYNC=False
-#
-# STEP 3:
-# -------
-# - In WingIDE, set debug environment
-#     To display yaml output without running a highstate (state.show.sls)
-#     Debug -> Debug Environment -> Environment --> --local --out=yaml state.show_sls users-test
-# - Or just run a highstate
-#     Debug -> Debug Environment -> Environment --> --local state.highstate -l debug
-#
-# STEP 4:
-# -------
-# - Set breakpoint in src directory
-#   not in:  /var/cache/salt/minion/extmods/...
-#
-################################################################################
+# To set up breakpoints in WingIDE, we need to softlink some stuff
 
-GITFS=/srv/salt/salt/files/master.d/gitfs.conf
+# Need to use salt git develop branch
+# In Wing...
+# Project -> Add Existing Directory ... /srv/salt-formulas
+# Project -> Project Properties -> Debug/Execute -> Main Entry Point -> /srv/salt-formulas/yamlscript-formula/src/salt-call
 
-CACHE_DIR=/var/cache/salt/minion
-EXTMODS="${CACHE_DIR}/extmods"
+# To display yaml output without running a highstate (state.show.sls)
+# Debug -> Debug Environment -> Environment --> --local --out=yaml state.show_sls users-test
+
+# Or just run a highstate
+# Debug -> Debug Environment -> Environment --> --local state.highstate -l debug
+#
+
+# IMPORTANT NOTE.  Be sure we are using the rootfs version, not gitfs or paths will be incorrect!
+# Set breakpoint on /var/cache/salt/minion/extmods/...
+
+#ln -sf /var/cache/salt/minion/extmods/renderers/yamlscript.pyc .
+#ln -sf /var/cache/salt/minion/extmods/utils/yamlscript_utils.pyc .
+#ln -sf /var/cache/salt/minion/extmods/utils/voluptuous.pyc .
 
 SRC="$(readlink -m .)"
-FORMULA_DIR="$(readlink -m ..)"
-NOW=$(date +"%Y-%m-%d:%H-%M-%S")
+EXTMODS="/var/cache/salt/minion/extmods"
 
-#
-# Need to update /srv/salt/salt/files/master.d/gitfs.conf to remove any
-# references to gitfs yamlscsript and add a rootsfs link directly to the 
-# development path
-#
-#mv "${GITFS}" "${GITFS}.${NOW}"
-cp ./gitfs.conf "${GITFS}"
-sed -i "s|SRC_DIR|${FORMULA_DIR}|" "${GITFS}"
-
-#
-# Make sure links in src directory point to actual source
-#
-rm voluptuous.py yamlscript.py yamlscript_utils.py
-ln -sf ../_utils/yamlscript_utils.py .
-ln -sf ../_utils/voluptuous.py .
-ln -sf ../_renderers/yamlscript.py .
-
-#
-# Then need to remove minion cache 
-#
-rm -rf "${CACHE_DIR}"/gitfs
-rm -rf "${CACHE_DIR}"/roots
-rm -rf "${CACHE_DIR}"/files
-rm -rf "${CACHE_DIR}"/extmods
-rm -rf "${CACHE_DIR}"/files
-rm -rf "${CACHE_DIR}"/file_lists
-
-#
-# Then run highstate to copy over all the new configurations
-#
-salt '*' state.highstate
-
-#
-# Setup softlinks
-#
 rm -f *.pyc
-#touch yamlscript.pyc
-#touch yamlscript_utils.pyc
-#touch voluptuous.pyc
+touch yamlscript.pyc
+touch yamlscript_utils.pyc
+touch voluptuous.pyc
 
 pushd "${EXTMODS}/renderers"
     rm -f yamscript.py?
